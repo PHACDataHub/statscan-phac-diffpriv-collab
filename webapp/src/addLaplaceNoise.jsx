ab6a87c5e0840640abfec7a6e48873f46c24a192
Siddhartha Haldar <sidhaldar98@gmail.com>
2023-11-28 21:34:56 -0800

    Fixed Laplace Noise Bug. Alse added correct config in launch.json for debugging

--- webapp/src/addLaplaceNoise.jsx
+++ webapp/src/addLaplaceNoise.jsx
@@ -2,7 +2,7 @@ const addLaplaceNoise = (originalValue, sensitivity, epsilon) => {
   const b = sensitivity / epsilon;
   const u = Math.random() - 0.5; // Uniform noise in the range [-0.5, 0.5]
   const noise = -1 * Math.sign(u) * b * Math.log(1 - 2 * Math.abs(u));
-  return originalValue + noise;
+  return parseFloat(originalValue) + noise;
 };
 
 export default addLaplaceNoise;
