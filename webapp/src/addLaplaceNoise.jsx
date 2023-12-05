aa195bb96682749eb87a3f05acd362cc7bdc3533
Siddhartha Haldar <sidhaldar98@gmail.com>
2023-12-05 08:35:31 -0800

    RModiifed Dockerfile

--- webapp/src/addLaplaceNoise.jsx
+++
@@ -1,8 +0,0 @@
-const addLaplaceNoise = (originalValue, sensitivity, epsilon) => {
-  const b = sensitivity / epsilon;
-  const u = Math.random() - 0.5; // Uniform noise in the range [-0.5, 0.5]
-  const noise = -1 * Math.sign(u) * b * Math.log(1 - 2 * Math.abs(u));
-  return parseFloat(originalValue) + noise;
-};
-
-export default addLaplaceNoise;
