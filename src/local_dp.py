cb0c92a0d886faad9e2a3d68b10d5136028ee055
Saptarshi Dutta Gupta <saptarshiduttagupta@gmail.com>
2023-12-05 05:13:23 +0000

    modify local_dp to pass epsilon instead of scale and probability

--- src/local_dp.py
+++ src/local_dp.py
@@ -20,6 +20,7 @@ from tqdm import tqdm as tqdm_func
 
 enable_features("contrib")
 
+np.random.seed(123)
 
 class LocalDifferentialPrivacy:
     """
@@ -27,23 +28,26 @@ class LocalDifferentialPrivacy:
     using Laplace noise and randomized response.
 
     Attributes:
-    - scale: The scale parameter for Laplace noise.
-    - probability: The probability parameter for randomized response.
+    - epsilon: The privacy parameter representing the amount of noise to be added.
 
     Methods:
     - define_input_space: Define the input space for measurements.
     - add_laplace_noise: Add Laplace noise to the input value.
-    - randomized_response: Add randomized response based
-                           on the type of variable.
-    - apply_local_dp: Apply local differential privacy to a given
-                      column of data based on its data type.
-    - apply_to_dataframe: Apply local differential 
-                          privacy to an entire dataframe.
+    - randomized_response: Add randomized response based on the type of variable.
+    - calculate_probability: Calculate probability using the sensitivity
+    - calculate_scale: Calculate Laplace scale using sensitivity
+    - apply_local_dp: Apply local differential privacy to a given column of data based on its data type.
+    - apply_to_dataframe: Apply local differential privacy to an entire dataframe.
+    - apply_query_to_list: Apply appropriate query function to a given list of data based on its data type and query type.
+    - apply_query_to_df: Apply appropriate query function to an entire dataframe.
+    - map_categories: Create a mapping from categories to numeric values.
+    - sensitivity_sum: Calculate sensitivity for the sum query on a numerical column.
+    - sensitivity_mean: Calculate sensitivity for the mean query on a numerical column.
+    - calculate_sensitivity: Calculate sensitivity for sum or mean queries on all columns of a dataframe.
     """
 
-    def __init__(self, scale: float, probability: float):
-        self.scale = scale
-        self.probability = probability
+    def __init__(self, epsilon: float):
+        self.epsilon = epsilon
 
     def define_input_space(
         self, input_space_type: str = "vector", variable_type: str = "float"
@@ -72,6 +76,7 @@ class LocalDifferentialPrivacy:
         self,
         input_space: Tuple[Domain, float],
         value: Union[str, float, int],
+        scale: float,
         variable_type: str = "float",
     ) -> Union[str, float, int]:
         """
@@ -80,6 +85,7 @@ class LocalDifferentialPrivacy:
         Parameters:
         - input_space: Input space for the measurement.
         - value: The true value to which noise is added.
+        - scale: The scale parameter for Laplace noise.
         - variable_type: Type of variable ('float', 'int', etc.).
 
         Returns:
@@ -87,81 +93,118 @@ class LocalDifferentialPrivacy:
         """
         if variable_type == "int":
             base_discrete_lap = make_base_discrete_laplace(
-                *input_space, scale=self.scale
+                *input_space, scale=scale
             )
             noisy_value = base_discrete_lap(value)
         elif variable_type == "float":
-            base_lap = make_base_laplace(*input_space, scale=self.scale)
+            base_lap = make_base_laplace(*input_space, scale=scale)
             noisy_value = base_lap(value)
         else:
             raise ValueError("Unsupported variable type")
         return noisy_value
 
     def randomized_response(
-        self, true_value: List[Union[str, float, int]], variable_type: str = "bool"
+        self, true_value: List[Union[str, float, int]], probability: float, variable_type: str = "bool" 
     ) -> List[Union[str, float, int]]:
         """
         Function to add local noise depending on the type of variable
         Paramaters:
             - true_value: list of values into which noise is injected
+            - probability: probability parameter for randomized response.
             - variable_type: type of variable [Possible options: bool, categorical]
         Returns:
             - noisy_value: list with the noise injected
         """
         if variable_type == "bool":
-            rr_measure = make_randomized_response_bool(prob=self.probability)
+            rr_measure = make_randomized_response_bool(prob=probability)
         elif variable_type == "categorical":
             rr_measure = make_randomized_response(
-                list(set(true_value)), prob=self.probability
+                list(set(true_value)), prob=probability
             )
         else:
             raise ValueError("Unsupported variable type")
         noisy_value = [rr_measure(value) for value in true_value]
         return noisy_value
 
-    def apply_local_dp(self, column: pd.Series) -> List[Union[str, float, int]]:
+    def calculate_probability(self, sensitivity: float) -> float:
+        """
+        Calculate probability using the sensitivity
+
+        Parameters:
+        - sensitivity: The sensitivity of the query.
+
+        Returns:
+        - probability: The calculated probability.
+        """
+
+        exponent = self.epsilon / sensitivity
+        probability = np.exp(exponent) / (1 + np.exp(exponent))
+        return probability
+
+    def calculate_scale(self, sensitivity: float) -> float:
+        """
+        Calculate Laplace scale using sensitivity
+
+        Parameters:
+        - sensitivity: The sensitivity of the query.
+
+        Returns:
+        - scale: The Laplace noise scale.
+        """
+        # Calculate Laplace noise scale
+        scale = sensitivity / self.epsilon
+        return scale
+
+    
+    def apply_local_dp(self, column: pd.Series, sensitivity_dict: Dict[str, float]) -> List[Union[str, float, int]]:
         """
         Apply local differential privacy to a given column 
         of data based on its data type.
 
         Parameters:
         - column: A pandas Series representing the data column.
+        - sensitivity_dict: A dictionary mapping column names to sensitivity values.
 
         Returns:
         - noisy_result: The column with local differential 
                         privacy applied.
         """
         if column.dtype == "category":
+            probability = self.calculate_probability(sensitivity_dict[column.name])
             noisy_result = self.randomized_response(
-                column.tolist(), variable_type="categorical"
+                column.tolist(), probability, variable_type="categorical"
             )
         elif column.dtype == "int":
+            scale = self.calculate_scale(sensitivity_dict[column.name])
             input_space = self.define_input_space(
                 input_space_type="vector", variable_type="int"
             )
             noisy_result = self.add_laplace_noise(
-                input_space, column.tolist(), variable_type="int"
+                input_space, column.tolist(), scale, variable_type="int"
             )
         elif column.dtype == "float":
+            scale = self.calculate_scale(sensitivity_dict[column.name])
             input_space = self.define_input_space(
                 input_space_type="vector", variable_type="float"
             )
             noisy_result = self.add_laplace_noise(
-                input_space, column.tolist(), variable_type="float"
+                input_space, column.tolist(), scale, variable_type="float"
             )
         elif column.dtype == "bool":
-            noisy_result = self.randomized_response(column.tolist())
+            probability = self.calculate_probability(sensitivity_dict[column.name])
+            noisy_result = self.randomized_response(column.tolist(), probability)
         else:
             return column
 
         return noisy_result
 
-    def apply_to_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
+    def apply_to_dataframe(self, df: pd.DataFrame, sensitivity_dict: Dict[str, float]) -> pd.DataFrame:
         """
         Apply local differential privacy to an entire dataframe.
 
         Parameters:
         - df: The input dataframe.
+        - sensitivity_dict: A dictionary mapping column names to sensitivity values.
 
         Returns:
         - df_transformed: The dataframe with local differential privacy applied.
@@ -172,7 +215,7 @@ class LocalDifferentialPrivacy:
             bar_format="{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]",
         )
 
-        return df.progress_apply(self.apply_local_dp)
+        return df.progress_apply(self.apply_local_dp, sensitivity_dict=sensitivity_dict)
 
 
     def apply_query_to_list(
@@ -289,20 +332,58 @@ class LocalDifferentialPrivacy:
         )
 
         return category_mapping, numeric_list
+    
+    
+    def sensitivity_sum(self, column_values) -> float:
+        """
+        Calculate sensitivity for the sum query on a numerical column.
 
+        Parameters:
+        - column_values: The values of the numerical column.
+
+        Returns:
+        - sensitivity: The sensitivity for the sum query.
+        """
+        return abs(max(column_values) - min(column_values))
+
+    def sensitivity_mean(self, column_values) -> float:
+        """
+        Calculate sensitivity for the mean query on a numerical column.
+
+        Parameters:
+        - column_values: The values of the numerical column.
+
+        Returns:
+        - sensitivity: The sensitivity for the mean query.
+        """
+        return abs(max(column_values) - min(column_values)) / len(column_values)
+    
+    def calculate_sensitivity(self, df: pd.DataFrame, query_type: str) -> Dict[str, float]:
+        """
+        Calculate sensitivity for sum or mean queries on all columns of a dataframe.
+
+        Parameters:
+        - df: The input dataframe.
+        - query_type: Type of query ("sum" or "mean").
+
+        Returns:
+        - sensitivity_dict: A dictionary mapping column names to sensitivity values.
+        """
+        sensitivity_dict = {
+            column: 1 if df[column].dtype == 'category' else
+                    self.sensitivity_sum(df[column].values) if query_type == 'sum' else
+                    self.sensitivity_mean(df[column].values)
+            for column in df.columns
+        }
+        return sensitivity_dict
 
 def main():
-    np.random.seed(42)
     
     parser = argparse.ArgumentParser(
         description="Apply local differential privacy to a dataset."
     )
-    parser.add_argument("--scale", type=float, help="Scale parameter for Laplace noise")
-    parser.add_argument(
-        "--probability",
-        type=float,
-        help="Probability parameter for randomized response",
-    )
+    parser.add_argument("--epsilon", type=float, help="Amount of noise to be added")
+    
     parser.add_argument(
         "--query_type",
         type=str,
@@ -320,8 +401,7 @@ def main():
     args = parser.parse_args()
 
     if (
-        not args.scale
-        or not args.probability
+        not args.epsilon
         or not args.input_csv
         or not args.output_csv
     ):
@@ -355,10 +435,12 @@ def main():
 
     df[columns_to_convert] = df[columns_to_convert].astype("category")
 
-    privacy = LocalDifferentialPrivacy(args.scale, args.probability)
-
+    privacy = LocalDifferentialPrivacy(args.epsilon)
+    
+    sensitivity_dict = privacy.calculate_sensitivity(df, args.query_type)
+    
     try:
-        df_transformed = privacy.apply_to_dataframe(df)
+        df_transformed = privacy.apply_to_dataframe(df, sensitivity_dict)
     except Exception as e:
         logging.error(f"Error applying local differential privacy: {e}")
         return
