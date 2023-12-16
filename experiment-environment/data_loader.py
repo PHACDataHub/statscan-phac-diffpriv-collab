0d08256e2842caadd4f4fe557d1eda3c00a1fa39
Saptarshi Dutta Gupta <saptarshiduttagupta@gmail.com>
2023-12-16 07:06:50 +0000

    fix dataloader for weights

--- experiment-environment/data_loader.py
+++ experiment-environment/data_loader.py
@@ -27,24 +27,20 @@ def load_data_sources(bucket_name: str, prefix: str, delimiter: str = "/", file_
     bucket = client.get_bucket(bucket_name)
     # Retrieve all blobs with the specified blob filters
     blobs = list(bucket.list_blobs(prefix=prefix, delimiter=delimiter))
-    
     blob_dfs = []
     # For each blob, load any which is a .csv file
     for blob in blobs:
-        if (len(blob.name) > 0 and '.csv' in blob.name and 'weights' not in blob.name):
+        if (len(blob.name) > 0 and '.csv' in blob.name and 'weighted' not in blob.name):
             blob_dfs.append(pd.read_csv(file_prefix + bucket_name + delimiter + blob.name))
-    
     # Create a single dataframe combining all within blob_dfs
     if len(blob_dfs) > 1:
         blob_df = pd.concat(blob_dfs, axis=1)
     else:
         blob_df = blob_dfs[0]
     blob_dfs = None
-    
     # load the weights file
     weights_df = pd.read_csv(file_prefix + bucket_name + delimiter + weights_filepath)
     # Rename the weights column name to match that of the main files
     weights_df = weights_df.rename(columns={"Unnamed: 0": "ID"})
-    
     # Return the merged weight and data files
     return pd.merge(blob_df, weights_df, on="ID", how="left")
