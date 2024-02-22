a8f8dd245bf3b241d9f41f5845cb674c59e24ca9
Saptarshi Dutta Gupta <saptarshiduttagupta@gmail.com>
2024-02-22 19:19:26 +0000

    add functions for calculating absolute difference

--- experiment-environment/evaluate.py
+++ experiment-environment/evaluate.py
@@ -4,6 +4,8 @@ from sdmetrics.reports.single_table import QualityReport
 import plotly.io as pio
 from IPython.display import HTML
 
+import utilities
+
 def generate_metadata(df, primary_key: str=None):
     """
     Generate metadata required for the quality report
@@ -65,8 +67,28 @@ def print_report_details(report):
     print(report.get_details(property_name='Column Shapes'))
 
 
-def evaluate_synthetic_dataset(df, df_transformed, filepath, dp_type):
-    
+def evaluate_synthetic_dataset(df: pd.DataFrame, 
+                               df_transformed: pd.DataFrame, 
+                               filepath: str, 
+                               dp_type: str, 
+                               quality_report_filename: str, 
+                               column_shape_filename: str, 
+                               column_pair_trends_filename: str) -> tuple:
+    """
+    Evaluate the quality of a synthetic dataset and generate visualizations.
+
+    Parameters:
+        df (pd.DataFrame): The original (real) dataset.
+        df_transformed (pd.DataFrame): The synthetic dataset transformed using a differential privacy method.
+        filepath (str): The directory path where evaluation results and visualizations will be saved.
+        dp_type (str): The type of differential privacy method used.
+        quality_report_filename (str): The filename for the quality evaluation report.
+        column_shape_filename (str): The filename for the visualization of column shapes.
+        column_pair_trends_filename (str): The filename for the visualization of column pair trends.
+
+    Returns:
+        tuple: A tuple containing the visualizations for column shapes and column pair trends.
+    """
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     
@@ -87,11 +109,11 @@ def evaluate_synthetic_dataset(df, df_transformed, filepath, dp_type):
     fig_pairs = quality_report.get_visualization(property_name='Column Pair Trends')
     
     # Save the report
-    quality_report.save(os.path.join(filepath, f'{dp_type}_quality_report.pkl'))
+    quality_report.save(os.path.join(filepath, f'{dp_type}{quality_report_filename}'))
     
     # save the figures
-    fig_shapes.write_html(os.path.join(filepath, f'{dp_type}_shape.html'))
-    fig_pairs.write_html(os.path.join(filepath, f'{dp_type}_columns_pair_trends.html'))
+    fig_shapes.write_html(os.path.join(filepath, f'{dp_type}{column_shape_filename}'))
+    fig_pairs.write_html(os.path.join(filepath, f'{dp_type}{column_pair_trends_filename}'))
     
     return fig_shapes, fig_pairs
 
@@ -117,43 +139,94 @@ def compare_query_results(real_result, synthetic_result):
     }
 
 
-def get_absolute_error(original_df: pd.DataFrame, modified_df: pd.DataFrame, filepath: str, filename: str, dp_type: str) -> None:
-    
+def get_absolute_error(original_df: pd.DataFrame, modified_df: pd.DataFrame, filepath: str, filename: str, dp_type: str, original_type: str) -> None:
     """
-    Calculate the absolute error between the original dataframe and the masked versions.
+    Calculate the absolute error between the original dataframe and the modified dataframe.
 
     Parameters:
-    df (DataFrame): The original dataframe containing sensitive information.
-    ldp_data_full (DataFrame): The dataframe representing the Local Differential Privacy (LDP) masked dataset.
-    sdp_data_full (DataFrame): The dataframe representing the Secure Differential Privacy (SDP) masked dataset.
+        original_df (DataFrame): The original dataframe containing sensitive information.
+        modified_df (DataFrame): The dataframe representing the modified dataset (e.g., LDP or SDP masked dataset).
+        filepath (str): The directory path where the CSV file will be saved.
+        filename (str): The name of the CSV file.
+        dp_type (str): The type of differential privacy method used (e.g., ldp_ or sdp_).
+        original_type (str): prefix for the original data (e.g., 'original_' for the original data).
 
     Returns:
-    None: The function saves the absolute error results for both LDP and SDP datasets to separate CSV files.
-    
-    Note: The function assumes that the 'ID' column is present in the dataframes and is used for sorting.
+        None: The function saves the absolute error results for both original and modified datasets to a CSV file.
+        
+    Note: 
+        The function assumes that the 'ID' column is present in the dataframes and is used for sorting.
+        It also assumes that the 'WTS_M' column is present in the dataframes and is dropped before calculating the absolute error.
     """
     
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     
-    # Get Absolute error results
+    # Get list of columns excluding 'ID' and 'WTS_M'
     col_list = original_df.columns.tolist()
     col_list = [col for col in col_list if col not in ['ID', 'WTS_M']]
     
-    
+    # Sort dataframes by 'ID' and drop 'ID' and 'WTS_M' columns
     original_df = original_df.sort_values(by='ID', ascending=True).reset_index(drop=True).drop(['ID', 'WTS_M'], axis=1)
     modified_df = modified_df.sort_values(by='ID', ascending=True).reset_index(drop=True).drop(['ID', 'WTS_M'], axis=1)
     
+    # Calculate absolute error for each column
     for column in col_list:
         modified_df['absolute_error_'+column] = abs(original_df[column] - modified_df[column])
         
-    original_df = original_df.add_prefix('original_')
+    # Add prefixes to column names
+    original_df = original_df.add_prefix(original_type)
     modified_df = modified_df.add_prefix(dp_type)
     
     # Concatenate the dataframes along columns axis
     combined_df = pd.concat([original_df, modified_df], axis=1)
     
-    # Sort columns
-    combined_df = combined_df.reindex(sorted(combined_df.columns), axis=1)
+    # Arrange the dataframe in desired format    
+    combined_df = utilities.arrange_columns(combined_df)
+
+    # Reset index and save to CSV
+    combined_df.reset_index(drop=True).to_csv(os.path.join(filepath, filename), index=False)
     
-    combined_df.reset_index(drop=True).to_csv(os.path.join(filepath, filename))
+    
+def get_absolute_error_queries(df_original: pd.DataFrame, 
+                               df_dp: pd.DataFrame, 
+                               dp_type: str, 
+                               original_prefix: str, 
+                               results_dir: str, 
+                               filename: str) -> None:
+    """
+    Calculate the absolute error between the original dataframe and the masked (differentially private) dataframe for specific queries.
+
+    Parameters:
+        df_original (pd.DataFrame): The original dataframe.
+        df_dp (pd.DataFrame): The dataframe representing the masked (differentially private) dataset.
+        dp_type (str): The type of differential privacy method used (e.g., 'ldp_' or 'sdp_').
+        original_prefix (str): prefix for the original data (e.g., 'original_' for the original data).
+        results_dir (str): The directory path where the CSV file will be saved.
+        filename (str): The name of the CSV file.
+
+    Returns:
+        None: The function saves the absolute error results for specific queries to a CSV file.
+    """
+    column_list = df_original.columns.tolist()
+    column_list.remove(query_type_column)
+
+    # Calculate absolute error for each relevant column
+    for column in column_list:
+        try:
+            df_dp['absolute_error_' + column] = abs(df_original[column] - df_dp[column])
+        except KeyError:
+            continue
+
+    # Add prefixes to column names
+    df_original = df_original.add_prefix(original_prefix)
+    df_dp = df_dp.add_prefix(dp_type)
+
+    # Concatenate the dataframes along columns axis
+    df_combined = pd.concat([df_original, df_dp], axis=1)
+
+    # Arrange the dataframe in desired format
+    df_combined = utilities.arrange_columns(df_combined)
+    
+    # Reset index and save to CSV
+    df_combined.reset_index(drop=True).to_csv(os.path.join(results_dir, filename), index=False)
